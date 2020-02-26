519b3cec0b78100330c2619e84849d1c96792c4d
Xiaobin Gao <XGao@grt.granitenet.com>
2020-02-26 17:28:36 -0500

    uploading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { StmtModifier } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DIA-angular';
+  
+  formdata;
+  error;
+
+  cid;
+  ip;
+
+  ngOnInit() {
+    this.formdata = new FormGroup({
+      ipt: new FormControl('')
+    });
+  }
+
+  onRdoChanged(obj, ipt, smt) {
+    this.cid == null;
+    this.ip == null;
+    this.error = false;
+    ipt.value = '';
+    ipt.placeholder = 'Circuit_ID';
+    if(obj.id == 'down') {
+      smt.value = 'Go Check';
+      smt.blur();
+    } else {
+      smt.value = 'Next';
+      smt.blur();
+    }
+  }
+
+  onSmtClicked(ipt, smt) {
+    // console.log(this.formdata.value);
+    if(ipt.value == '') {
+      this.error = true;
+    } 
+    else {
+      this.error = false;
+      if(this.cid == null) {
+        this.cid = ipt.value;
+      }
+      else {
+        this.ip = ipt.value;
+        ipt.placeholder = 'X.X.X.X';
+        ipt.value = '';
+        smt.value = 'Go Check';
+        smt.blur();
+      }
+      if(this.cid != null && this.ip != null) {
+        // TODO: send request out
+      }
+
+    }
+    console.log(this.cid, this.ip)
+  }
+  
 }
